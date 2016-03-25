894572de5cb264c0a3bdc9e7cd796ebf058cf667
Łukasz Wojciechowski <lukasz.wojciechowski@stxnext.pl>
2016-03-25 08:52:28 +0100

    Fix tabs :)

--- app/chat.service.ts
+++ app/chat.service.ts
@@ -6,9 +6,9 @@ const CHAT_URL = 'ws://localhost:8080/chat';
 
 @Injectable()
 export class ChatService {
-	public messages: Subject<Message>;
+    public messages: Subject<Message>;
 
-	constructor(wsService: WebSocketService) {
-		this.messages = wsService.connect(CHAT_URL);
-	}
+    constructor(wsService: WebSocketService) {
+        this.messages = wsService.connect(CHAT_URL);
+    }
 }
--- app/websocket.service.ts
+++ app/websocket.service.ts
@@ -2,52 +2,52 @@ import Rx from 'rxjs/Rx';
 import {Injectable} from 'angular2/core';
 
 export interface Message {
-	author: string;
-	message: string;
+    author: string;
+    message: string;
 }
 
 @Injectable()
 export class WebSocketService {
-	private subject: Rx.Subject<Message>;
+    private subject: Rx.Subject<Message>;
 
-	public connect(url): Rx.Subject<Message> {
-		if(!this.subject) {
-			this.subject = this.create(url);
-		}
+    public connect(url): Rx.Subject<Message> {
+        if(!this.subject) {
+            this.subject = this.create(url);
+        }
 
-		return this.subject;
-	}
+        return this.subject;
+    }
 
-	private create(url): Rx.Subject<Message> {
-		let ws = new WebSocket(url);
+    private create(url): Rx.Subject<Message> {
+        let ws = new WebSocket(url);
 
-		let observable = Rx.Observable.create((obs: Rx.Observer<Message>) => {
-			ws.onmessage = (evt: MessageEvent) => obs.next(this.transformResponse(evt));
-			ws.onerror = obs.error.bind(obs);
-			ws.onclose = obs.complete.bind(obs);
+        let observable = Rx.Observable.create((obs: Rx.Observer<Message>) => {
+            ws.onmessage = (evt: MessageEvent) => obs.next(this.transformResponse(evt));
+            ws.onerror = obs.error.bind(obs);
+            ws.onclose = obs.complete.bind(obs);
 
-			return ws.close.bind(ws);
-		});
+            return ws.close.bind(ws);
+        });
 
-		let observer = {
-			next: (data: Message) => {
-				if (ws.readyState === WebSocket.OPEN) {
-					ws.send(JSON.stringify(data));
-				}
-			},
-		};
+        let observer = {
+            next: (data: Message) => {
+                if (ws.readyState === WebSocket.OPEN) {
+                    ws.send(JSON.stringify(data));
+                }
+            },
+        };
 
-		return Rx.Subject.create(observer, observable);
-	}
+        return Rx.Subject.create(observer, observable);
+    }
 
-	private transformResponse(response: MessageEvent): Message {
-		let data = JSON.parse(response.data);
+    private transformResponse(response: MessageEvent): Message {
+        let data = JSON.parse(response.data);
 
-		let message = {
-			author: data.author,
-			message: data.message,
-		}
+        let message = {
+            author: data.author,
+            message: data.message,
+        }
 
-		return message;
-	}
+        return message;
+    }
 }
